4730a78ab327289d91145cdac80899137d86cdcd
Erick Kiminza <kiminzaeric@gmail.com>
2022-02-21 22:33:05 +0300

    fetch json data and map it to html

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
-async function getGroup () {
-    try {
-      const response1 = await fetch('https://kiminza.pythonanywhere.com/Join_Group/')
-      const datas = await response1.json()
-      for (const data of datas) {
-        console.log(data.group_name)
-        const response2 = await fetch(data.group_name)
-        const names = await response2.json()
-        console.log(names.group_name)
-        optionText = names.group_name;
-        optionValue = data.group_name;
-        $('#form-control').append(new Option(optionText, optionValue))
-      }
-    }
-    catch (error) {
-      console.error(error)
-    }
-}
-getGroup()
+// async function getGroup () {
+//     try {
+//       const response1 = await fetch('https://kiminza.pythonanywhere.com/Join_Group/')
+//       const datas = await response1.json()
+//       for (const data of datas) {
+//         console.log(data.group_name)
+//         const response2 = await fetch(data.group_name)
+//         const names = await response2.json()
+//         console.log(names.group_name)
+//         optionText = names.group_name;
+//         optionValue = data.group_name;
+//         $('#form-control').append(new Option(optionText, optionValue))
+//       }
+//     }
+//     catch (error) {
+//       console.error(error)
+//     }
+// }
+// getGroup()
 
 function showForm() {
     document.getElementById('my-Form').style.display = 'block';
+}
+
+fetch('https://kiminza.pythonanywhere.com/homes/')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data)
+})
+.catch (function (error) {
+    console.error(error);
+});
+
+function appendData (data) {
+    const mainContainer = document.getElementById("myData");
+    for (const data_ of data){
+        const div = document.createElement("div");
+        div.innerHTML = data_.home_name;
+        mainContainer.append(div)
+    }
 }
 
 // This code activates flatpickr on fields with the 'datetimefield' class when the document has loaded
