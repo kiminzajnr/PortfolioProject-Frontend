369d76ead4f6156f7efcc22549fb7c9885ead25b
Erick Kiminza <kiminzaeric@gmail.com>
2022-02-22 15:51:40 +0300

    .

--- js/index.js
+++ js/index.js
@@ -35,15 +35,15 @@ fetch('https://kiminza.pythonanywhere.com/homes/')
 
 function appendData (data) {
     // const mainContainer = document.getElementById("myData");
+    const div = document.createElement("div");
+    div.className = "card";
     for (const data_ of data){
-        const div = document.createElement("div");
-        div.className = "card";
         div.innerHTML += `<h3><b> ${data_.home_name} </b></h3>`;
         div.innerHTML += `<h5><b> ${data_.contact} </b></h5>`;
         div.innerHTML += `<h5><b> ${data_.location} </b></h5>`;
         div.innerHTML += `<h5><b> ${data_.capacity} </b></h5>`;
-        document.body.appendChild(div)   
     }
+    document.body.appendChild(div)
 }
 
 // This code activates flatpickr on fields with the 'datetimefield' class when the document has loaded
